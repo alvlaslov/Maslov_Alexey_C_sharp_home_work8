c4fb4ee8971cd4786e7e6207b3b2ea37bd575930
Alexey Maslov <alvladigital@gmail.com>
2022-08-21 11:38:55 +0300

    add second method task54

--- Task54/Program.cs
+++ Task54/Program.cs
@@ -8,63 +8,133 @@
 //7 4 2 1
 //9 5 3 2
 //8 4 4 2
-
-Random random = new Random();
-int rows = random.Next(3, 5);
-int columns = random.Next(3, 5);
-int[,] array = new int[rows, columns];
-FillArray(array);
-PrintArray(array);
-SortArray(array);
-Console.WriteLine("Descending ordered elements of each row of the two-dimensional array: ");
-PrintArray(array);
-
-void FillArray(int[,] array, int startnumber = 0, int finishnumber = 10)
+void FirstMethod()
 {
-    finishnumber++;
     Random random = new Random();
-    int rows = array.GetLength(0);
-    int columns = array.GetLength(1);
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < columns; j++)
-        {
-            array[i, j] = random.Next(startnumber, finishnumber);
-        }
-    }
-}
+    int rows = random.Next(3, 5);
+    int columns = random.Next(3, 5);
+    int[,] array = new int[rows, columns];
+    FillArray(array);
+    PrintArray(array);
+    SortArray(array);
+    Console.WriteLine("Descending ordered elements of each row of the two-dimensional array: ");
+    PrintArray(array);
 
-void PrintArray(int[,] array)
-{
-    int rows = array.GetLength(0);
-    int columns = array.GetLength(1);
-    for (int i = 0; i < rows; i++)
+    void FillArray(int[,] array, int startnumber = 0, int finishnumber = 10)
     {
-        for (int j = 0; j < columns; j++)
+        finishnumber++;
+        Random random = new Random();
+        int rows = array.GetLength(0);
+        int columns = array.GetLength(1);
+        for (int i = 0; i < rows; i++)
         {
-            Console.Write(array[i, j] + "\t");
-        }
-        Console.WriteLine();
-    }
-}
-
-void SortArray(int[,] array)
-{
-    int rows = array.GetLength(0);
-    int columns = array.GetLength(1);
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < columns; j++)
-        {
-            for (int k = 0; k < columns - 1; k++)
+            for (int j = 0; j < columns; j++)
             {
-                if (array[i, k] < array[i, k + 1])
+                array[i, j] = random.Next(startnumber, finishnumber);
+            }
+        }
+    }
+
+    void PrintArray(int[,] array)
+    {
+        int rows = array.GetLength(0);
+        int columns = array.GetLength(1);
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write(array[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    void SortArray(int[,] array)
+    {
+        int rows = array.GetLength(0);
+        int columns = array.GetLength(1);
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                for (int k = 0; k < columns - 1; k++)
                 {
-                    int temp = array[i, k + 1];
-                    array[i, k + 1] = array[i, k];
-                    array[i, k] = temp;
+                    if (array[i, k] < array[i, k + 1])
+                    {
+                        int temp = array[i, k + 1];
+                        array[i, k + 1] = array[i, k];
+                        array[i, k] = temp;
+                    }
                 }
             }
         }
     }
 }
+
+
+
+void SecondMethod()
+{
+    Random random = new Random();
+    int rows = random.Next(3, 5);
+    int columns = random.Next(3, 5);
+    int[,] array = new int[rows, columns];
+    FillArray(array);
+    PrintArray(array);
+    SortArray(array);
+    Console.WriteLine("Descending ordered elements of each row of the two-dimensional array: ");
+    PrintArray(array);
+
+    void FillArray(int[,] array, int startnumber = 0, int finishnumber = 10)
+    {
+        finishnumber++;
+        Random random = new Random();
+        int rows = array.GetLength(0);
+        int columns = array.GetLength(1);
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                array[i, j] = random.Next(startnumber, finishnumber);
+            }
+        }
+    }
+
+    void PrintArray(int[,] array)
+    {
+        int rows = array.GetLength(0);
+        int columns = array.GetLength(1);
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write(array[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    void SortArray(int[,] array)
+    {
+        int rows = array.GetLength(0);
+        int columns = array.GetLength(1);
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = columns - 1; j > 0; j--)
+            {
+                for (int k = 0; k < j; k++)
+                {
+                    if (array[i, k] < array[i, k + 1])
+                    {
+                        int temp = array[i, k];
+                        array[i, k] = array[i, k + 1];
+                        array[i, k + 1] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
+
+FirstMethod();
+SecondMethod();
